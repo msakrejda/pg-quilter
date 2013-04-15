47456d7ed25b164bb0a47c4c55dd07fc2cd4fb3a
Maciek Sakrejda <m.sakrejda@gmail.com>
2013-04-14 20:53:36 -0700

    Schema tweaks

--- migrations/20130323160000_initial_schema.rb
+++ migrations/20130323160000_initial_schema.rb
@@ -3,8 +3,8 @@ Sequel.migration do
     create_table :topics do
       uuid :uuid, default: 'uuid_generate_v4()'.lit, primary_key: true
       text :name, null: false
-      text :pull_request
-      timestamptz :created_at, default: 'now()'.lit
+      text :pull_request, null: false
+      timestamptz :created_at, null: false, default: 'now()'.lit
       text :committed_in
     end
 
@@ -12,7 +12,7 @@ Sequel.migration do
       uuid :uuid, default: 'uuid_generate_v4()'.lit, primary_key: true
       foreign_key :topic_id, :topics
       text :message_id, null: false
-      timestamptz :created_at, default: 'now()'.lit
+      timestamptz :created_at, null: false, default: 'now()'.lit
     end
 
     create_table :patches do
@@ -25,10 +25,10 @@ Sequel.migration do
     create_table :applications do
       uuid :uuid, default: 'uuid_generate_v4()'.lit, primary_key: true
       foreign_key :patch_id, :patches
-      text :base_sha
-      boolean :succeeded
-      text :output
-      timestamptz :created_at, default: 'now()'.lit
+      text :base_sha, null: false
+      boolean :succeeded, null: false
+      text :output, null: false
+      timestamptz :created_at, null: false, default: 'now()'.lit
     end
   end
 end
