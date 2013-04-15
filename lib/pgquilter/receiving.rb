ea325a02e3a7c66de79b5e3497be2c2eb1c45fce
Maciek Sakrejda <m.sakrejda@gmail.com>
2013-04-14 21:26:44 -0700

    Update message processing notes

--- lib/pgquilter/receiving.rb
+++ lib/pgquilter/receiving.rb
@@ -1,14 +1,12 @@
 module PgQuilter
   class Receiving
     def process(msg)
-      # 1. Check if message is a topic message
+      # 1. Check if any relevant attachments (attachments are going to
+      #    be: text/x-diff, text/x-patch, application/x-gzip, ?)
       # 2. Find (or create) topic
-      # 3. Create patchset
-      # 4. For each attachment
-      #    a) download
-      #    b) unzip if necessary
-      #    c) create patch
-      # 5. Schedule patchset build
+      # 3. Create Patchset
+      # 4. Create Patch for each attachment (unzip first if necessary)
+      # 5. Schedule topic build
     end
   end
 end
