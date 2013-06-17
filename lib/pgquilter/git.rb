b52d2d9ccc346b88d7046844c9bc60274c529d98
Maciek Sakrejda <maciek@heroku.com>
2013-06-17 00:01:36 -0700

    Split out git wrapper class

--- lib/pgquilter/git.rb
+++ lib/pgquilter/git.rb
@@ -1,45 +1,39 @@
+# -*- coding: utf-8 -*-
 module PGQuilter
   class Git
 
-    def check_upstream_sha
-      (git "ls-remote #{PGQuilter::Config::CANONICAL_REPO_URL}").split("\t").first
+    def initialize(harness)
+      @g = harness
     end
 
-    def git_reset
-      git "checkout master"
-      git "fetch upstream"
-      git "reset --hard upstream/master"
-      @base_sha = git("show-ref -s refs/heads/master").chomp
+    def check_upstream_sha
+      @g.check_upstream_sha
     end
 
     def apply_patchset(patchset)
       # TODO: report failure
       check_workspace
-      git_reset
-      git "checkout -B #{patchset.topic.name}"
-      # TODO: it would be useful to create a commit moving us to master branch
-      # without actually losing commit history
-      git "reset --hard master"
+      @g.reset
+      @g.prepare_branch patchset.topic.name
+
+      # TODO: track whether any patch has been successfully applied
       patchset.patches.sort_by(&:patchset_order).each do |patch|
         apply_patch(patch)
       end
       # TODO: better commit message, e.g., referencing the actual
       # patch e-mails (by link or at least ID)
-      git "commit . --author='#{author}' -m 'Applying patch set for #{patchset.topic.name}'"
+      @g.git_commit("Applying patch set for #{patchset.topic.name}", author)
     end
 
     def push_to_github(patchset)
       # push both master and the patch so we always have the latest PR
-      git "push origin master"
-      git "cherry-pick travis-config"
-      git "push -f origin #{patchset.topic.name}"
-      patchset.topic.name
+      branch = patchset.topic.name
+      @g.update_branch(branch)
     end
 
     def submit_pull_request(branch)
       # for now, do this only on the first patchset (later, add comments
       # for each subsequent patchset)
-      # TODO: different class?
       github = Github.new(login: PGQuilter::Config::GITHUB_USER,
                           password: PGQuilter::Config::GITHUB_PASSWORD)
       github.pull_requests.create(user, 'postgres',
@@ -49,50 +43,8 @@ module PGQuilter
                                     "base" => "master" })
     end
 
-    def run_cmd(cmd)
-      # We ignore stderr for now; we're likely never to need it here
-      result = `#{cmd}`
-      unless $?.exitstatus == 0
-        raise StandardError, "Command '#{cmd}' failed"
-      end
-      result
-    end
-
-    def git(cmd)
-      result = nil
-      FileUtils.cd(PGQuilter::Config::WORK_DIR) do
-        result = run_cmd "git #{cmd}"
-      end
-      result
-    end
-
-    def has_workspace?
-      File.directory? PGQuilter::Config::WORK_DIR
-    end
-
     def check_workspace
-      prepare_workspace unless has_workspace?
-    end
-
-    def prepare_workspace
-      ssh_setup
-      git_clone
-      git_setup
-    end
-
-    def ssh_setup
-      run_cmd 'echo "$GITHUB_PRIVATE_KEY" > $HOME/.ssh/id_rsa'
-    end
-
-    def git_setup
-      git "config --global user.name '#{PGQuilter::Config::QUILTER_NAME}'"
-      git "config --global user.email '#{PGQuilter::Config::QUILTER_EMAIL}'"
-    end
-
-    def git_clone
-      run_cmd "mkdir -p #{PGQuilter::Config::WORK_DIR}"
-      git "clone #{PGQuilter::Config::WORK_REPO_URL} #{PGQuilter::Config::WORK_DIR}"
-      git "remote add upstream #{PGQuilter::Config::CANONICAL_REPO_URL}"
+      @g.prepare_workspace unless @g.has_workspace?
     end
 
     def apply_patch(patch)
@@ -102,10 +54,13 @@ module PGQuilter
       File.open(patch_path, 'w') do |patch_file|
         patch_file.write patch.body
       end
-      # TODO: does a failed application produce stderr output?
-      result = git "apply #{patch_path}"
-      PGQuilter::Application.create(base_sha: @base_sha, patch_id: patch.uuid,
-                                    succeeded: result.success?, output: result)
+      # TODO: this is kind of a gross hack around patch application
+      result = @g.apply_patch(patch_path)
+      # TODO: properly detect successful patch application
+      failed =~ /does not apply\Z/
+      patch.add_application(base_sha: @base_sha,
+                            succeeded: failed.nil?, output: result)
+      failed.nil?
     end
 
   end
---
+++ lib/pgquilter/git_harness.rb
@@ -0,0 +1,83 @@
+module PGQuilter
+  class GitHarness
+
+    def self.check_upstream_sha
+      git("ls-remote #{PGQuilter::Config::CANONICAL_REPO_URL} master").split("\t").first
+    end
+
+    def run_cmd(cmd)
+      # We ignore stderr for now; we're likely never to need it here
+      # N.B.: this is super-unsafe; don't run with untrusted input
+      result = `#{cmd}`
+      unless $?.exitstatus == 0
+        raise StandardError, "Command '#{cmd}' failed"
+      end
+      result
+    end
+
+    def git(cmd)
+      result = nil
+      FileUtils.cd(PGQuilter::Config::WORK_DIR) do
+        result = run_cmd "git #{cmd}"
+      end
+      result
+    end
+
+    def reset
+      git "checkout master"
+      git "fetch upstream"
+      git "reset --hard upstream/master"
+      @base_sha = git("show-ref -s refs/heads/master").chomp
+    end
+
+    def prepare_branch(branch)
+      # TODO: it would be useful to create a commit moving us to master branch
+      # without actually losing commit history
+      git "checkout -B '#{branch}'"
+      git "reset --hard master"
+    end
+    
+    def has_workspace?
+      File.directory? PGQuilter::Config::WORK_DIR
+    end
+
+    def prepare_workspace
+      ssh_setup
+      git_clone
+      git_setup
+    end
+
+    def ssh_setup
+      run_cmd 'echo "$GITHUB_PRIVATE_KEY" > $HOME/.ssh/id_rsa'
+    end
+
+    def git_setup
+      git "config --global user.name '#{PGQuilter::Config::QUILTER_NAME}'"
+      git "config --global user.email '#{PGQuilter::Config::QUILTER_EMAIL}'"
+    end
+
+    def git_clone
+      run_cmd "mkdir -p #{PGQuilter::Config::WORK_DIR}"
+      git "clone #{PGQuilter::Config::WORK_REPO_URL} #{PGQuilter::Config::WORK_DIR}"
+      git "remote add upstream #{PGQuilter::Config::CANONICAL_REPO_URL}"
+    end
+
+    def update_branch(branch)
+      git "push origin master"
+      git "cherry-pick travis-config"
+      git "push -f origin #{branch}"
+    end
+
+    def apply_patch(patch_path)
+      git "apply --verbose #{patch_path} 2>&1 || true"
+    end
+
+    def git_commit(message, author)
+      # TODO: fix escaping / handle parameters properly
+      message.gsub!("'", "''")
+      author.gsub!("'", "''")
+      git "commit . -m '#{message}' --author='#{author}'"
+    end
+
+  end
+end
