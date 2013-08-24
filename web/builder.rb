9d32052a5b5519846d6c6d871bd91ea9df95b930
Maciek Sakrejda <maciek@heroku.com>
2013-08-24 16:29:03 -0700

    Add GET endpoint for builds

--- web/builder.rb
+++ web/builder.rb
@@ -4,7 +4,7 @@ require 'mail'
 
 class PGQuilter::Builder < Sinatra::Base
   # validate and store a build request; to be picked up by a worker
-  post '/build' do
+  post '/builds' do
     payload = JSON.parse request.body.read
 
     base_sha = payload.delete "base_sha"
@@ -18,5 +18,22 @@ class PGQuilter::Builder < Sinatra::Base
       PGQuilter::TaskMaster.create_build(base_sha, patches)
       200
     end
+  end
+
+  get '/builds/:uuid' do |uuid|
+    b = PGQuilter::Build[uuid]
+    if b.nil?
+      status 404
+    end
+    {
+      id: uuid,
+      created_at: b.created_at,
+      patches: b.patches.sort_by(&:order).map do |p|
+        {
+          sha1: Digest::SHA1.hexdigest(p.body)
+        }
+      end,
+      status: 'pending'
+    }.to_json
   end
 end
