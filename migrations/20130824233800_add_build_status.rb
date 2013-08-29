41da53e451f9557d4927e3545dd6c95606a22acc
Maciek Sakrejda <maciek@heroku.com>
2013-08-28 22:22:09 -0700

    Add BuildStep migration

---
+++ migrations/20130824233800_add_build_status.rb
@@ -0,0 +1,22 @@
+Sequel.migration do
+  change do
+    self.execute <<-EOF
+    CREATE TYPE build_step AS ENUM('reset', 'apply',
+        'configure', 'make', 'make check',
+        'make contribcheck');
+EOF
+
+    create_table :build_step do
+      uuid :uuid, default: 'uuid_generate_v4()'.lit, primary_key: true
+      foreign_key :build_id, :builds, type: :uuid, null: false
+      build_step :step, null: false
+      timestamptz :started_at, null: false
+      timestamptz :completed_at, null: false, default: 'now'
+      text :stdout, null: false
+      text :stderr, null: false
+      int :status, null: false
+      hstore :attrs
+    end
+
+  end
+end
