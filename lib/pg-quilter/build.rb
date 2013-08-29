a42c166611f91b682d43142a118b5275c5365e28
Maciek Sakrejda <maciek@heroku.com>
2013-08-28 22:00:04 -0700

    Add build steps

--- lib/pg-quilter/build.rb
+++ lib/pg-quilter/build.rb
@@ -3,9 +3,14 @@ require 'sequel'
 module PGQuilter
   class Build < Sequel::Model
     one_to_many :patches
+    one_to_many :build_steps
   end
 
   class Patch < Sequel::Model
+    many_to_one :builds
+  end
+
+  class BuildStep < Sequel::Model
     many_to_one :builds
   end
 end
