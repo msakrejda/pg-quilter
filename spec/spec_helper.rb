e5966d3eaf1b8a341916c8ca9639f3226b300e8b
Maciek Sakrejda <maciek@heroku.com>
2013-08-31 13:46:18 -0700

    Purge unused config blocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,18 +7,6 @@ RSpec.configure do |config|
   config.order = 'random'
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
-  config.before(:each) do
-
-  end
-
-  config.before(:suite) do
-
-  end
-
-  config.after(:each) do
-
-  end
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
