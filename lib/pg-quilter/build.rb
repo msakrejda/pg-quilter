9ebeb04bdb8c6518f48c265aa1e53017652ed7d1
Maciek Sakrejda <maciek@heroku.com>
2013-08-31 13:49:11 -0700

    Add method for picking an unbuilt build

--- lib/pg-quilter/build.rb
+++ lib/pg-quilter/build.rb
@@ -4,6 +4,44 @@ module PGQuilter
   class Build < Sequel::Model
     one_to_many :patches
     one_to_many :build_steps
+
+    # Run the given block with the first unbuilt build locked to
+    # protect against multiple concurrent builds. If there is no
+    # unbuilt build, this method does not yield to the block, so the
+    # block can always assume its build argument is non-nil.
+    def self.with_first_unbuilt
+      return unless block_given?
+      DB.transaction(:isolation => :serializable, :retry_on=>[Sequel::SerializationFailure]) do
+        # Find something that has no build steps--N.B.: this will
+        # *not* automatically rebuild interrupted builds
+        build = self.first_unbuilt.all.first
+        unless build.nil?
+          yield(build)
+        end
+      end
+    end
+
+    private
+
+    dataset_module do
+      def first_unbuilt
+        select(Sequel.*(:builds))
+          .with_sql(<<-EOF)
+SELECT
+  builds.*
+FROM
+  builds LEFT OUTER JOIN build_steps ON (builds.uuid = build_steps.build_id)
+WHERE
+  build_steps.uuid IS NULL
+ORDER BY
+  builds.created_at
+LIMIT
+  1
+FOR UPDATE OF
+  builds
+EOF
+      end
+    end
   end
 
   class Patch < Sequel::Model
