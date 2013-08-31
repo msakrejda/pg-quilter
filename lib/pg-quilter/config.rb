4290f1d54bf04ed1c671d570badd47d97978491e
Maciek Sakrejda <maciek@heroku.com>
2013-08-31 15:00:36 -0700

    Purge unused config settings

--- lib/pg-quilter/config.rb
+++ lib/pg-quilter/config.rb
@@ -1,20 +1,7 @@
 module PGQuilter
   module Config
-    LOG_LEVEL = ENV['LOG_LEVEL']
-
-    QUILTER_NAME = 'pg-quilter'
-    QUILTER_EMAIL = ENV['QUILTER_EMAIL']
-
-    GITHUB_USER = 'pg-quilter'
-    GITHUB_PASSWORD = ENV['GITHUB_PASSWORD']
-
-    PGSQL_HACKERS = 'pgsql-hackers'
-    HACKERS_ARCHIVE = 'http://www.postgresql.org/message-id'
-
     # N.B.: this must be something that is accessible when fakesu-ing
     WORK_DIR = '/app/postgres'
-    BAD_PATCH_SENTINEL = '.pg-quilter-patch-application-failed.sentinel'
-    WORK_REPO_URL = 'git@github.com:pg-quilter/postgres.git'
     UPSTREAM_REPO_URL = 'git@github.com:postgres/postgres.git'
   end
 end
